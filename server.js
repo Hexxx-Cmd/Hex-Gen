c594c59d6745cba529e46ac811a59fe7309849d3
Glitch (hello-express) <none>
2020-04-07 18:26:33 +0000

    👑⛵️ Checkpoint ./server.js:8155730/234 ./config.json:8155730/44

--- server.js
+++ server.js
@@ -2,6 +2,13 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 const config = require("./config.json")
+const prefix = config.perfix
 const token = config.token
+const themedir = config.ThemesFolder
+const theme = config.selectedTheme
+
 client.login(token)
-//ok where
+
+
+
+//o forgot the project u still here??
