537fbbeb2c5d24410c2d283a9f6e1d2b8dd6d483
Glitch (hello-express) <none>
2020-04-07 18:50:28 +0000

    💝🍡 Checkpoint ./server.js:8155730/522

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const prefix = config.perfix
 const token = config.token
 const themedir = config.ThemesFolder
 const theme = config.selectedTheme
-
 client.login(token)
 
-
+if(theme == "trash")
     const alts = [];
 client.on("message", msg =>{
   const devs = ["",
@@ -17,7 +16,6 @@ client.on("message", msg =>{
   let args = msg.content.split(" ").slice(1)[0];
   let cmd = msg.content.split(' ')[0]
   if(cmd === `${prefix}add`){
-  let roleW = msg.mentions.roles.first();
   if(!devs.includes(msg.author.id)){
     let embed = new Discord.RichEmbed()
     .setColor("#42f4f4")
@@ -25,21 +23,20 @@ client.on("message", msg =>{
     msg.reply(embed).then( z => z.delete(3000));
      return
   }
-  let role = args;
-  if(!role) {
+  let altarg = args;
+  if(!altarg) {
     let embed = new Discord.RichEmbed()
     .setColor("#42f4f4")
     .setTitle(`:x:error1.`);
   msg.reply(embed).then( msgs => msgs.delete(3000));
   return
   }
-  var randomkeys = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var gift = "";
-  for (var y = 0; y < 16; y++) {   ///16
-    gift +=  `${randomkeys.charAt(Math.floor(Math.random() * randomkeys.length))}`;
-  }
+   ///16
+    gift +=  args;
+  
  
-  alts[gift] = role;
+  alts[gift] = altarg;
   let embed = new Discord.RichEmbed()
   .setColor("#42f4f4")
   .setTitle(`:ok_hand: - **Code sent in DMs**`);
@@ -47,9 +44,7 @@ client.on("message", msg =>{
   let embed2= new Discord.RichEmbed()
   .setAuthor(msg.author.username, msg.author.displayAvatarURL)
   .setThumbnail(msg.author.avatarURL)
-  .addField("**Account/s added!**", gift,true)
-  .addField("Type", "MC Alt")
-  .addField("This Key Made by", msg.author, true)
+  .setTitle("Successfully added!")
   .setTimestamp()
   .setFooter(client.user.username,client.user.displayAvatarURL);
   msg.author.send(embed2);
