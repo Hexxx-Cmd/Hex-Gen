b11b21b4d94692a10e407640a75d860b90b0a248
Glitch (hello-express) <none>
2020-04-08 09:43:05 +0000

    🖐🎤 Checkpoint ./j4jshits.js:6220285/904:8155730/4097

--- j4jshits.js
+++ j4jshits.js
@@ -5,43 +5,62 @@ lmfao this shit YEAH
 */
 
 // Package's //
-const shit = require("discord.js")
-const j4jshit = new shit.Cient()
-const Fuck = require("fs")
-const request = require("request")
-const token = ""
+const shit = require("discord.js");
+const j4jshit = new shit.Cient();
+const Fuck = require("fs");
+const request = require("request");
+const token = "";// we need to get random math e soz wait oki'll write it
+const username = ["", "","","",""]
+const avatars = ["", "","","",""]
+// Auto Name's + Avatar's. \\
+shit.on('ready', function() {
+    shit.user.setUsername(); // give me some j4j servers (active ones)
+    shit.user.setAvatar();
+  // Auto-Join \\ EZ
+  JoinShit("j4j");
+  JoinShit("FP5Ttsa");
+});
 // Config \\
 const Gay = ""; // Ur gay Invite Link
-function JoinShit(invite_code){
-request.post(`https://discordapp.com/api/invites/${invite_code}`, {
-  headers: {
-      "Authorization": token
-  }
-}, (error, response, body) => {
-   if(error) {
-       console.log(error);
-       return;
-   }
+function JoinShit(invite_code) {
+  request.post(
+    `https://discordapp.com/api/invites/${invite_code}`,
+    {
+      headers: {
+        Authorization: token
+      }
+    },
+    (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
 
-   console.log(body, response.statusMessage, response.statusCode);
-})
+      console.log(body, response.statusMessage, response.statusCode);
+    }
+  );
 }
 
 /*
-Scoure Code.
+
+Message's Holder.
 
 */
 shit.on("message", message => {
-  
-  if(message.content.Includes("j4j")) {
-  message.author.send("j4j")
-    message.author.send(Gay)
-  
+  if (message.content.Includes("j4j")) {
+    message.author.send("j4j");
+    message.author.send(Gay);
   } //the function is joinShit
-  
 });
+
 shit.on("message", message => {
-  if(message.content.Includes("Done")) {
-  message.channel.send("Done I joined")
+  if (message.content.Includes("discord.gg")) {
   }
-  });
+});
+
+shit.on("message", message => {
+  if (message.content.Includes("Done")) {
+    message.channel.send("Done I joined");
+  }
+});
+// Custom Thing'y \\ what's it wait coding it
