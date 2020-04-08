ec4ce3ddc5bb49187a0f9704aacb099ccb906af4
Glitch (hello-express) <none>
2020-04-08 08:31:39 +0000

    🎥🐽 Checkpoint ./server.js:6220285/1729:8155730/1051 ./config.json:6220285/114

--- server.js
+++ server.js
@@ -1,12 +1,55 @@
-//Generator bot by RealShadow#2020 DarkBoy#6171
+/*
+
+Generator Bot.
+
+Developer's: RealShadow__#2020 + DarkBoy#6171
+- Please Don't Steal Credit's.
+Today Date: Wednesday 8 Apr 2020
+*/
+      // Packages \\
 const Discord = require("discord.js")
 const client = new Discord.Client()
 const config = require("./config.json")
+     // Config \\
 const prefix = config.perfix
 const token = config.token
 const themedir = config.ThemesFolder
-const theme = config.selectedTheme                              
+const theme = config.selectedTheme           
+const ChangeStatusTime = config.changestatus
+const name = config.name // Bot Name 
+const welcomeid = config.welcomeid
 client.login(token)
+const activities_list = [
+    `${name} New Generator Bot!`, 
+    `${prefix}help | ${prefix}invite`,
+    `Watching: ${client.guilds.size} Servers`, 
+    `Need Help? ${prefix}Support`
+    ]; 
+    // Status \\
+client.on('ready', () => {
+console.log('Bot is on')
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); 
+        client.user.setActivity(activities_list[index]); 
+    }, ChangeStatusTime); 
+});
+
+
+
+
+//Chat here\\
+/*
+Sooo 
+Ok                        
+mm let's add the normal commands for now
+like help and ban and these things
+ ban these things? 
+ brb 10 mins afk
+  */
+
+
+
+           // Scoure Code \\ 
 
 
 
@@ -14,3 +57,6 @@ client.login(token)
 
 
 
+
+
+    // Welcome Scoure \\
