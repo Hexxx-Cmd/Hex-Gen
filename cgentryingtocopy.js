83a5319473666800d362fbc3ea936399d55704b9
Glitch (hello-express) <none>
2020-04-08 17:52:11 +0000

    👾🏩 Checkpoint ./cgentryingtocopy.js:8155730/1726

--- cgentryingtocopy.js
+++ cgentryingtocopy.js
@@ -6,3 +6,21 @@ dark.on("ready", function() {
 console.log("This bot is for daddy Hexxx🌙#2020 \n         Logged in as" + dark.user.tag + "\n Status: OK")
 console.log("unblock us xdddd")
 })
+const prefix = "!"
+const name = ""
+const premium = true
+const ChangeStatusTime = "50000"
+const CloowDown = "100000" 
+const activities_list = [
+    `${name} New Generator Bot!`, 
+    `${prefix}help | ${prefix}invite`,
+    `Watching: ${dark.guilds.size} Servers`, 
+    `Need Help? ${prefix}Support`
+    ]; 
+dark.on('ready', () => {
+  console.log('Bot is on')
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); 
+        dark.user.setActivity(activities_list[index]); 
+    }, ChangeStatusTime); 
+});
