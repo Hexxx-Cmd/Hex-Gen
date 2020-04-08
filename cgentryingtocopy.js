df4690d74d62ec7e9b475459e9b4e6030de47b4d
Glitch (hello-express) <none>
2020-04-08 17:41:05 +0000

    👍📇 Checkpoint ./cgentryingtocopy.js:8155730/330

--- cgentryingtocopy.js
+++ cgentryingtocopy.js
@@ -1,2 +1,8 @@
 const darkdevs = require("discord.js")
-const dark = darkdevs.Client()
+const dark = darkdevs.Client();
+
+
+dark.on("ready", function() {
+console.log("This bot is for daddy Hexxx🌙#2020 \n         Logged in as" + dark.user.tag + "\n Status: OK")
+console.log("unblock us xdddd")
+})
