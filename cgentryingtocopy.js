3a84157c0b4ced9dbb4b7d7efd1be8f036b2c805
Glitch (hello-express) <none>
2020-04-08 18:28:02 +0000

    🍏🚤 Checkpoint ./cgentryingtocopy.js:8155730/7808

--- cgentryingtocopy.js
+++ cgentryingtocopy.js
@@ -1,6 +1,13 @@
-const darkdevs = require("discord.js")
+const darkdevs = require("discord.js");
 const dark = darkdevs.Client();
-const pages = [`
+const prefix = "!";
+const name = "";
+const premium = true;
+const ChangeStatusTime = "50000";
+const CloowDown = "100000";
+const page = 1;
+const pages = [
+  `
 Donors Commands
 
 #lolgame - League of Legends account | #premium
@@ -24,7 +31,8 @@ Generator Commands
 #Invite - Invites The Bot To Your Server
 
 Check out https://zzhhzz.xyz/ for more infromation about this amazing bot!
-`,`Other Commands:
+`,
+  `Other Commands:
 
 #server - to see your server status
 #bans - to see how many members got banned in your server
@@ -35,18 +43,63 @@ Check out https://zzhhzz.xyz/ for more infromation about this amazing bot!
 #lock - Locks the channel in which this command is used. People will be able to see the message but not send a message
 #unlock - to unlock the channel
 #unlock - Unlocks the channel in which this command is used.
-`,``]
+`,
+  `C-Gen Premium Bot
+
+1 - Prime Features
+2 - Set a Channel For The Gen bot
+3 - Working only in Your Server
+4 - Change The Name and Picture
+5 - ×2 The The Amount of Accounts
+6 - #Setname
+7 - #Setgame/watching/stream
+9 - Set Premium Users Channel ! (You can Earn money from it ! ) Guaranteed
+
+=> 7.99 £ (1 Month)
+=> 14.99 £ (3 Months) Save 1 month
+=> 31.99 £ (1 Year) Save 8 months ! & earn money from Selling Premium User , earn 1000/100
+
+
+C-Gen Prime Bot
+
+1 - Without ads
+2 - Amount of Accounts For Your Server
+3 - On a Private Server (Host)
+4 - Streaming (Your Server name)
+5 - Setname
+6 - Setgame/watching/stream
+
+=> 5.99 £ (1 Month)
+=> 9.99 £ (3 Months) Save 1 month
+=> 22.99 £ (1 Year) Save 8 months !`,
+  `Donors Features
+
+Access The
+- Steam Accounts
+- Disney+ Accounts
+- League of Legends Accounts
+
+=> 3.99£ (Lifetime)
+
+Join the official Server and contact any staff member
+https://www.cgen.xyz/
+`
+];
+
+
 
 dark.on("ready", function() {
-console.log("This bot is for daddy Hexxx🌙#2020 \n         Logged in as" + dark.user.tag + "\n Status: OK")
-console.log("unblock us xdddd")
-})
-const prefix = "!"
-const name = ""
-const premium = true
-const ChangeStatusTime = "50000"
-const CloowDown = "100000" 
-dark.on('ready', () => {
-  console.log('Bot is on')
-        dark.user.setActivity("#help | cgen.xyz"); 
+  console.log(
+    "This bot is for daddy Hexxx🌙#2020 \n         Logged in as" +
+      dark.user.tag +
+      "\n Status: OK"
+  );
+  console.log("unblock us xdddd");
 });
+
+dark.on("ready", () => {
+  console.log("Bot is on");
+  dark.user.setActivity("#help | cgen.xyz");
+});
+
+dark.on("message", re => {});
