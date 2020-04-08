63e6f96b474834f864dc3dd299bbdbeb1e0e90b2
Glitch (hello-express) <none>
2020-04-08 09:17:40 +0000

    🚘⚽️ Checkpoint ./j4jshits.js:6220285/1475:8155730/1125 ./package.json:6220285/1336

---
+++ j4jshits.js
@@ -0,0 +1,31 @@
+//Copy right ur mom don't copy Darkboy#6171/RealShadow#2020 made this
+/*
+Best Code LOL
+lmfao this shit YEAH
+*/
+
+// Package's //
+const shit = require("discord.js")
+const j4jshit = new shit.Cient()
+const Fuck = require("fs")
+// Config \\
+const Gay = ""; // Ur gay Invite Link
+
+// okay
+//ez
+
+shit.on("message", message => {
+  
+  if(message.content.Includes("j4j")) {
+  message.author.send("j4j").then(gayinvite => message.author.send(Gay))
+  
+  } // wait should we make it join too so alt or guy wont leave? what's the api for the join ???
+  // i have made one in my Tool ill just copy scoure 
+// send me the api i'll make it here 
+  
+});
+shit.on("message", message => {
+  if(message.content.Includes("Done")) {
+  message.channel.send("Done I joined")
+  }
+  });
