899c6c7ff5380f0d2c83d8c3b0d1556a5a03dccf
Glitch (lazy-developer) <none>
2020-04-09 08:22:05 +0000

    💴🌔 Checkpoint ./genconfig.json:8155730/2 ./commands/fortnite.js:8155730/587 ./commands/help.js:8155730/4894

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -1,16 +1,12 @@
 const Discord = require('discord.js')
 
 module.exports.run = (bot, message, args, gen) => {
-    if(!gen.hasAccess(message.member)) return message.channel.send({embed:{title:`You don't have permission to use gen!`, color:bot.color}})
-    if(!gen.allowedChannel(message.channel)) return message.channel.send({embed:{title:`Gen can't be used in this channel!`, color:bot.color}})
     if (bot.cooldown.has(message.author.id)) {
-            message.channel.send({embed:{title:`Please wait 5 minutes!`, color:bot.color}})
+            message.channel.send({embed:{title:`Please wait 15 minutes!`, color:bot.color}})
     } else {
-    let type = args[0]
-    if(!type) return message.channel.send({embed:{title:`Usage: ${bot.prefix}gen <type>`, color:bot.color}})
-    if(type==='free') return message.channel.send({embed:{title:`Please use ${bot.prefix}freegen`, color:bot.color}})
+    let type = "fortnite"
     let alt = gen.getAlt(type)
-    if(!alt || alt === '') return message.channel.send({embed:{title:`We don't have those accounts in stock!`, color:bot.color}})
+    if(!alt || alt === '' || alt === ',') return message.channel.send({embed:{title:`We don't have those accounts in stock!`, color:bot.color}})
     let embed = new Discord.RichEmbed()
     .setColor(bot.color)
     .setTitle('Gen')
@@ -18,7 +14,6 @@ module.exports.run = (bot, message, args, gen) => {
     .addField('Email',alt.split(':')[0],true)
     .addField('Password',alt.split(':')[1],true)
     message.author.send(embed)
-    gen.removeAlt(type,alt)
     message.channel.send({embed:{title:`Account has been sent successfully!`, color:bot.color}})
     bot.cooldown.add(message.author.id);
         setTimeout(() => {
---
+++ commands/help.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+
+module.exports.run = (bot, message, args, gen) => {
+
+}
+
+module.exports.help = {
+    name: 'help',
+    aliases: ['help']
+}
