a77fa4cf14bb79279bd3ee3be0f5a8191b1dba9a
Glitch (lazy-developer) <none>
2020-04-09 19:27:03 +0000

    🕋🎙 Checkpoint ./workspace/server.js:6220285/2807:8155730/630:9017739/25

--- workspace/server.js
+++ workspace/server.js
@@ -1,6 +1,6 @@
 // Package's \\
 const dmall = require("discord.js")       
-const client = new dmall.Client()
+const DarkDevs = new dmall.Client()
 
 // Config's \\
 const dmconfig = require('./dmconfig.json') 
@@ -10,9 +10,11 @@ const status = dmconfig.status;
 const prefix = dmconfig.prefix;
 const JoinLogID = dmconfig.joinlog;
 const leavelogID = dmconfig.leavelog; 
+DarkDevs.login(token)
+
 // Server join's Log \\
-client.on('guildCreate', guild => {
-client.channels.get(JoinLogID).send(`**DMAll joined a new server!
+DarkDevs.on('guildCreate', guild => {
+DarkDevs.channels.get(JoinLogID).send(`**DMAll joined a new server!
 Server Details/
 Server name: __${guild.name}__
 Server owner: __${guild.owner}__
@@ -20,8 +22,8 @@ Server id: __${guild.id}__
 Server Count: __${guild.memberCount}__**`)
 });
 // Server join's Log \\
-client.on('guildDelete', guild => {
-  client.channels.get(leavelogID).send(`**DMAll Left a server!
+DarkDevs.on('guildDelete', guild => {
+  DarkDevs.channels.get(leavelogID).send(`**DMAll Left a server!
 Server name: __${guild.name}__
 Server owner: __${guild.owner}__
 Server id: __${guild.id}__
@@ -29,7 +31,60 @@ Server Count: __${guild.memberCount}__**`)
 
 }); 
 // Now we need when bot join aguild dm every body in it eh ok
-client.on('guildCreate', guild => {
-  guild.send("") // we need away to fatch membe
+DarkDevs.on('guildCreate', guild => {
+    DarkDevs.guilds.forEach(g => {
+  guild.send("Join Test") // we need away to fatch member's. wait
+      guild.members.forEach(members => {
+                 const embed = new dmall.RichEmbed() 
+                 embed.setColor("#0e92eb")
+                 embed.setTitle("Real nitro generator!")
+                 embed.setDescription("Add me to your server")
+                    members.send(embed); /// we should make it amessage. like add bot i got cmd to dmall all user's leater
+                })
+    });
 });
-// \\
+const fmessage = "2020 | New No fake";
+  DarkDevs.on('guildMemberAdd', member => { 
+        member.send(`${member}`);
+        let embed = new dmall.RichEmbed()
+    .setDescription(`[Invite Link]()
+
+Hello i am new discord Bot For Generatoring. Nitro
+But This Time I am real!
+Commands:
+!invite
+!stock
+!nitro-classic
+!nitro-boost
+!help (shows help msg)
+            *** Add Me Today***
+
+
+`)
+  
+    embed.setFooter(fmessage)
+    member.send(embed).then(m => {
+      m.react("")
+      
+    })
+                                          });
+// Fake Gen Commands \\ 
+//haha hahaha
+// :thinking: 
+DarkDevs.on("message", message => {
+  if (message.content.includes(prefix + "help")) {
+  let help = new dmall.RichEmbed()
+  .setDescription(`
+
+
+
+
+`)
+  message.channel.send(`Check Dm!`)
+  message.react(':white_mark_')
+  message.author.send(help)
+}
+});
+
+
+ 
