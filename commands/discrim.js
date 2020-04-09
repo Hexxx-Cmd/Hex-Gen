9fb81c7880aaa9c37ee363edd5888e172dc46037
Glitch (lazy-developer) <none>
2020-04-09 13:54:22 +0000

    👽🎷 Checkpoint ./commands/fortnite.js:7676787/592:8155730/990 ./commands/discrim.js:7676787/380:8155730/585 ./Team-Projects/json-files/J4j-Config.json:7676787/274:8155730/159

--- commands/discrim.js
+++ commands/discrim.js
@@ -7,7 +7,7 @@ exports.run = (bot, message, args, ops, PREFIX) => {
         .setColor(0xffffff);
     const embednodm = new Discord.RichEmbed()
     embednodm.setColor("#19c9ff")
-    embednodm.setTitle("C-Gen")
+    embednodm.setTitle("Hex-gen")
     embednodm.setDescription("Sorry, you can't use this commands in DMs")
     embednodm.setURL("https://zzhhzz.xyz/")
     if(message.channel.type === "dm") return message.channel.send(embednodm); 
@@ -15,12 +15,11 @@ exports.run = (bot, message, args, ops, PREFIX) => {
     if (isNaN(args[0]) || args[0] > 9999 || args[0] < 1) { // Run if out of parameters
          ///huh
         
-        embed.setAuthor(`C-Gen V3.0.8`, 'https://images-ext-1.discordapp.net/external/01_8RjAdHjEj34QLC2ZIQxyBM-WfFneoL9Feuj5iUIc/https/images-ext-1.discordapp.net/external/Tje-nMlp6hCc_mtPnMsOuzXM3OWiuHP7uAtPvduTGRU/%253Fv%253D1/https/cdn.discordapp.com/emojis/653666145353596950.gif')
-        embed.setThumbnail('https://images-ext-1.discordapp.net/external/N1lA0vHrw53obFclQOoD2aUQX886E5waMWJJs4UEdmk/https/i.imgur.com/n3fEsTa.gif?width=633&height=475')
-        embed.setDescription(`**Sorry, please enter a valid discrim. Usage: #discrim [#????]**\nWanna See The C-Gen Features ? try our Command #premium\nhttps://www.cgen.xyz/Premium`);
+        embed.setAuthor(`Hex-gen V3.0.8`, 'https://images-ext-1.discordapp.net/external/01_8RjAdHjEj34QLC2ZIQxyBM-WfFneoL9Feuj5iUIc/https/images-ext-1.discordapp.net/external/Tje-nMlp6hCc_mtPnMsOuzXM3OWiuHP7uAtPvduTGRU/%253Fv%253D1/https/cdn.discordapp.com/emojis/653666145353596950.gif')
+        embed.setThumbnail('https://media.discordapp.net/attachments/697287727447539782/697805418436755456/Tick.gif')
+        embed.setDescription(`**Sorry, please enter a valid discrim. Usage: #discrim [#????]**\nWanna See The Hex-gen Features ? [Try this](https://zzhhzz.xyz)`);
         embed.setURL("https://zzhhzz.xyz/")
-        ///-_-
-        // Send error message
+        //did i ask for any other details? hehe
         return message.channel.send(embed);
         
     }
--- commands/fortnite.js
+++ commands/fortnite.js
@@ -12,18 +12,20 @@ module.exports.run = (bot, message, args, gen) => {
     .setTitle('You have Generated Fortnite Account')
     .addField('**__Your account:__**', alt)
     .addField('**__Remind:__**',"Remember that you are using the free generator, this account may not work !",true)// remove this
-    .addField('**__C-Gen Premium__**',"You Have to Buy The Premium Version to get The Bot s Features, For You and Your Server",true)
-    .addField('**__Is C-Gen completely legal ?__**',"Yes it is! So many people are falsely claiming that our service is illegal or against some type of EULA or TOS, which is simply not true All of our accounts are provided by the specified accounts owners, bought at the original game site or bought from resellers. Dont believe what people falsely claim to make C-Gen look bad.")
-    .setFooter("C-Gen © 2017-2020, Simple Generator Bot - All Rights Reserved")
+    .addField('**__Hex-gen Premium__**',"You Have to Buy The Premium Version to get The Bot s Features, For You and Your Server",true)
+    .addField('**__Is Hex-gen completely legal ?__**',"Yes it is! So many people are falsely claiming that our service is illegal or against some type of EULA or TOS, which is simply not true All of our accounts are provided by the specified accounts owners, bought at the original game site or bought from resellers. Dont believe what people falsely claim to make Hex-gen look bad.")
+    .setFooter("Hex-Gen © 2020-2025, Simple Generator Bot - All Rights Reserved")
     message.author.send(embed)
       let ree = new Discord.RichEmbed()
     .setColor(bot.color)
-    .setTitle(`C-Gen`)
-    .setDes
-    .addField('You Have Generated a Fortnite Account Check Your DM.', `Wanna See The C-Gen Features ? try our Command \`\`#premium\`\``)
-    .setFooter("C-Gen © 2017-2020, Simple Generator Bot - All Rights Reserved")
-    .setURL("https://zzhhzz.xyz")
-    message.channel.send(ree)
+    .setThumbail('https://media.discordapp.net/attachments/697287727447539782/697805418436755456/Tick.gif')
+    .setAuthor(`Hex-gen V3.0.8`, 'https://images-ext-1.discordapp.net/external/01_8RjAdHjEj34QLC2ZIQxyBM-WfFneoL9Feuj5iUIc/https/images-ext-1.discordapp.net/external/Tje-nMlp6hCc_mtPnMsOuzXM3OWiuHP7uAtPvduTGRU/%253Fv%253D1/https/cdn.discordapp.com/emojis/653666145353596950.gif')
+    .setTitle(`Hex-Gen`) //yo the name is Hex-Gen not HexGenOkay
+    .setDescription("**You Have Generated a Fortnite Account Check Your DM.**")
+    .addDescription(`Wanna See The Hex-gen Features ? try our Command \`\`#premium\`\``) //ok now time to change everything too Hex gen ok?OK
+    .setFooter("Hex-Gen © 2020-2025, Simple Generator Bot - All Rights Reserved")
+    .setURL("https://zzhhzz.xyz")  
+    message.channel.send(ree) //lol shushNOU
     bot.cooldown.add(message.author.id);
         setTimeout(() => {
           bot.cooldown.delete(message.author.id);
