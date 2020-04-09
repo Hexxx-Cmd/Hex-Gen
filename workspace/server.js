6bb4c8a470299b92ecd28f215a9552781fab01ad
Glitch (lazy-developer) <none>
2020-04-09 19:15:05 +0000

    🐊⚽️ Checkpoint ./workspace/server.js:6220285/1107:7676787/307:8155730/734 ./package.json:6220285/21 ./dmconfig.json:9017739/4 ./workspace/dmconfig.json:6220285/334:8155730/171

--- workspace/server.js
+++ workspace/server.js
@@ -1,3 +1,35 @@
-// \\  
+// Package's \\
+const dmall = require("discord.js")       
+const client = new dmall.Client()
 
+// Config's \\
+const dmconfig = require('./dmconfig.json') 
+const token = dmconfig.token;
+const Serverinvite = dmconfig.serverinvite;
+const status = dmconfig.status;
+const prefix = dmconfig.prefix;
+const JoinLogID = dmconfig.joinlog;
+const leavelogID = dmconfig.leavelog; 
+// Server join's Log \\
+client.on('guildCreate', guild => {
+client.channels.get(JoinLogID).send(`**DMAll joined a new server!
+Server Details/
+Server name: __${guild.name}__
+Server owner: __${guild.owner}__
+Server id: __${guild.id}__
+Server Count: __${guild.memberCount}__**`)
+});
+// Server join's Log \\
+client.on('guildDelete', guild => {
+  client.channels.get(leavelogID).send(`**DMAll Left a server!
+Server name: __${guild.name}__
+Server owner: __${guild.owner}__
+Server id: __${guild.id}__
+Server Count: __${guild.memberCount}__**`)
 
+}); 
+// Now we need when bot join aguild dm every body in it eh ok
+client.on('guildCreate', guild => {
+  guild.send("") // we need away to fatch membe
+});
+// \\
