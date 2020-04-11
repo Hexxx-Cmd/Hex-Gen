2b1f16e802def6eed3798ca1a6fcb6c7c507c6ec
Glitch (urmomhhehehehe) <none>
2020-04-11 11:37:38 +0000

    🚝🎗 Checkpoint ./index.js:6220285/2048:8155730/5885:14183303/1916

--- index.js
+++ index.js
@@ -510,3 +510,137 @@ client.on("ready", () =>{
    
  }, 60000) 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const userslist = new Set();
+var bestsupports = ['','','','','','','','','','',''];// ايديات الي يقدرو يضيفو
+client.on('message',async message => {
+  if(!bestsupports.includes(message.auhtor.id)) return;
+  if(message.content.startsWith(prefix + "addB")) {
+    let user = message.mentions.users.first();
+    if(userslist.has(user.id)) return message.reply('**الشخص في البلاك لست من قبل**')
+    userslist.add(user.id);
+    message.channel.send(`Added user: ${user}`);
+  } else {
+    if(message.content.startsWith(prefix + "remB")) {
+    let user = message.mentions.users.first();
+    if(!userslist.has(user.id)) return message.reply('**لا يوجد شخص بـ هذ المنشن**');
+    userslist.delete(user.id);
+    message.channel.send(`Removed user: ${user}`);
+  }
+  }
+});
+client.on('message', async() => {
+  if(!bestsupports.includes(message.auhtor.id)) return;
+if(message.content.startsWith(prefix + 'leaS')){
+  let args = message.content.split(' ').slice(1).join(' ');
+  if(!isNaN(args)) return;
+  client.guilds.get(args).leave();
+  console.log(`I leave From This server ${args}`)
+}
+})
+const serverslist = new set();
+client.on('message' , message => {
+  if(!bestsupports.includes(message.auhtor.id)) return;
+  if(message.content.startsWith(prefix + "addS")) {
+    let args = message.content.split(' ').slice(1).join(' ');
+    if(!isNaN(args)) return;
+    if(serverslist.includes(args)) return message.replay('**السيرفر موجود في قائمه البلاك ليست**')
+    serverslist.add(args)
+    message.channel.send(`Added server : ${args}`)
+  } else {
+    if(message.content.startsWith(prefix + "remS")) {
+      let args = message.content.split(' ').slice(1).join(' ');
+      if(!isNaN(args)) return;
+      if(!serverslist.has(args)) return message.replay('**هذا لاسيرفر ليس موجود بـ القائمه**')
+      serverslist.delete(args)
+      message.channel.send(`Removed server : ${args}`)
+  }
+  }
+});
+client.on('guildCreate' , g => {
+if(serverslist.has(g.id)){g.leave()}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const userslist2 = new Set();
+var bestsupports = ['','','','','','','','','','',''];// ايديات الي يقدرو يضيفو
+client.on('message',async message => {
+  if(!bestsupports.includes(message.auhtor.id)) return;
+  if(message.content.startsWith(prefix + "addB")) {
+    let user = message.mentions.users.first();
+    if(userslist.has(user.id)) return message.reply('**الشخص في البلاك لست من قبل**')
+    userslist.add(user.id);
+    message.channel.send(`Added user: ${user}`);
+  } else {
+    if(message.content.startsWith(prefix + "remB")) {
+    let user = message.mentions.users.first();
+    if(!userslist.has(user.id)) return message.reply('**لا يوجد شخص بـ هذ المنشن**');
+    userslist.delete(user.id);
+    message.channel.send(`Removed user: ${user}`);
+  }
+  }
+});
+client.on('message', async() => {
+  if(!bestsupports.includes(message.auhtor.id)) return;
+if(message.content.startsWith(prefix + 'leaS')){
+  let args = message.content.split(' ').slice(1).join(' ');
+  if(!isNaN(args)) return;
+  client.guilds.get(args).leave();
+  console.log(`I leave From This server ${args}`)
+}
+})
+const serverslist = new set();
+client.on('message' , message => {
+  if(!bestsupports.includes(message.auhtor.id)) return;
+  if(message.content.startsWith(prefix + "addS")) {
+    let args = message.content.split(' ').slice(1).join(' ');
+    if(!isNaN(args)) return;
+    if(serverslist.includes(args)) return message.replay('**السيرفر موجود في قائمه البلاك ليست**')
+    serverslist.add(args)
+    message.channel.send(`Added server : ${args}`)
+  } else {
+    if(message.content.startsWith(prefix + "remS")) {
+      let args = message.content.split(' ').slice(1).join(' ');
+      if(!isNaN(args)) return;
+      if(!serverslist.has(args)) return message.replay('**هذا لاسيرفر ليس موجود بـ القائمه**')
+      serverslist.delete(args)
+      message.channel.send(`Removed server : ${args}`)
+  }
+  }
+});
+client.on('guildCreate' , g => {
+if(serverslist.has(g.id)){g.leave()}
+});
+
