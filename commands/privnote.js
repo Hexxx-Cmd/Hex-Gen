5964a1a63690941a50187a1cf26f77f8086d92e9
Glitch (urmomhhehehehe) <none>
2020-04-12 11:39:21 +0000

    👔🎟 Checkpoint ./commands/privnote.js:8155730/710

--- commands/privnote.js
+++ commands/privnote.js
@@ -3,10 +3,21 @@ module.exports.run = (bot, message, args, gen) => {
   let wrongusage = new Discord.RichEmbed()
   .setTitle(`**Incorrect usage**`)
   .setDescription(`#Usage: #privnote [Message]`)
-  .setFooter(`Hex Gen Premium`)
-    .setColor("GOLD");
-message.delete()
- const { createPrivnote, retrievePrivnote } = require('privnote');
+  .setFooter(`HexGen Premium`)
+  .setColor("GOLD");
+  let checkdms = new Discord.RichEmbed()
+  .setTitle(`**Successful**`)
+  .setDescription(`Link send in your DMs`)
+  .setFooter(`HexGen Premium`)
+  .setColor("GREEN");
+  let link = new Discord.RichEmbed()
+  .setTitle(`**Successful**`)
+  .setDescription(created.url)
+  .setURL()
+  .setFooter(`HexGen Premium`)
+  .setColor("GOLD");
+if(!args) return message.channel.send(wrongusage)
+ const { createPrivnote } = require('privnote');
 (async () => {
   const created = await createPrivnote(args);
   console.log("Privnote created! content:" + args);
