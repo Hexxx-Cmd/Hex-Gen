e0f7b2b16038324b70738ece5e26112fe352c759
Glitch (urmomhhehehehe) <none>
2020-04-12 11:27:52 +0000

    😺🏡 Checkpoint ./commands/privnote.js:8155730/385

--- commands/privnote.js
+++ commands/privnote.js
@@ -1,11 +1,16 @@
 const Discord = require('discord.js')
 module.exports.run = (bot, message, args, gen) => {
+  let wrongusage = new Discord.RichEmbed()
+  .setTitle(`**Incorrect usage**`)
+  .setDescription(`#Usage: #privnote [Message]`)
+  .setFooter(`Hex Gen Premium`)
+    .setColor("GOLD");
 message.delete()
  const { createPrivnote, retrievePrivnote } = require('privnote');
 (async () => {
   const created = await createPrivnote(args);
-  console.log(created);
-message.a
+  console.log("Privnote created! content:" + args);
+message.author.send(created.url)
 })()
 }
   
